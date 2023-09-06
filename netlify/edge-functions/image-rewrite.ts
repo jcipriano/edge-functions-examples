8532e3f29afe1e3a45d52557b56904624606b7ca
jcipriano <>
2023-09-06 13:41:20 -0700

    Adds edge function for image rewrite

---
+++ netlify/edge-functions/image-rewrite.ts
@@ -0,0 +1,16 @@
+import type { Context } from "https://edge.netlify.com";
+
+// Let's serve an image of a kitten from the internet
+
+export default async (request: Request, context: Context) => {
+  // fetch() is supported natively by Deno!
+  // Returning the awaited response automatically sets the
+  // content-type headers!
+  const kitten = await fetch("https://placekitten.com/g/300/300");
+  return kitten;
+};
+
+// catch all image requests
+export const config: Config = {
+    path: ["/*.png", "/*.jpg"]
+}
